94cd3192f7e11b8e5d60ed5d6240fbb8388f093c
indianbhaiya <61813271+indianbhaiya@users.noreply.github.com>
2020-04-02 12:48:12 +0530

    Initial commit

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,19 @@
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
+
+@command(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("`Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
+                     f"`My peru owner`: {DEFAULTUSER}\n\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @pureindialover\n"
+                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
+                     "[Deploy this userbot Now](https://github.com/indianbhaiya/IndianBot)\n\n"
+                     "[Bot Creator](http://t.me/pureindialover)")
