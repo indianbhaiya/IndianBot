8fe8c1615ba22da17aeeb21d8e3a0952768d9d5d
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-04-08 18:23:35 +0530

    Update gitcommit.py

--- userbot/plugins/gitcommit.py
+++ userbot/plugins/gitcommit.py
@@ -13,11 +13,10 @@ import time
 from datetime import datetime
 from telethon import events
 from telethon.tl.types import DocumentAttributeVideo
-
+from userbot.utils import admin_cmd
 
 GIT_TEMP_DIR = "./userbot/temp/"
-
-#@borg.on(pattern="^.commit", outgoing=True)
+#@command(pattern="^.commit", outgoing=True)
 @borg.on(admin_cmd(pattern=r"commit"))
 async def download(event):
     if event.fwd_from:
