7f3aab177e26b5838eb86b6ae13cb07e44c84d12
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-04-08 21:54:41 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -64,7 +64,7 @@ async def _(event):
            packname = f"IndianBhaikeStickers" 
            packshortname = "IndianBhai_ke_locker_me" 
         else:
-        packname = f"{userid}'s kang pack" 
+            packname = f"{userid}'s kang pack" 
             packshortname = f"IndianBot_{userid}_stickers" # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
