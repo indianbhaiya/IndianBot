83467b341c2d82ac0e3e9c6c9457dff7fb2fdaeb
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-04-08 17:02:17 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -11,7 +11,8 @@ class Var(object):
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
     # Here for later purposes
-    SUDO_USERS = os.environ.get("SUDO_USERS", "719877937")
+    SUDO_USERS = os.environ.get("SUDO_USERS", "953414679")
+    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", None))
     LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
