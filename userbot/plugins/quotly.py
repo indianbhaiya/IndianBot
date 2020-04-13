43b4515e05459e80f34e915eac5e0f458b193f35
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-04-13 13:57:52 +0530

    Update quotly.py

--- userbot/plugins/quotly.py
+++ userbot/plugins/quotly.py
@@ -1,39 +1,47 @@
 """QuotLy: Avaible commands: .qbot
-Thank You to @pureindialover for this plugin
 """
-import datetime
 from telethon import events
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from telethon.tl import functions, types
 from uniborg.util import admin_cmd
 
 @borg.on(admin_cmd(pattern="qbot ?(.*)"))
 async def _(event):
     if event.fwd_from:
-        return 
-    if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
-       return
-    reply_message = await event.get_reply_message() 
-    if not reply_message.text:
-       await event.edit("```Reply to text message```")
-       return
-    chat = "@QuotLyBot"
-    sender = reply_message.sender
-    if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
-       return
-    await event.edit("```Making a Quote```")
-    async with event.client.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=953414679))
-              await event.client.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("```Please unblock me (@QuotLyBot) u Nigga```")
-              return
-          if response.text.startswith("Hi!"):
-             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
-          else: 
-             await event.delete()
-             await event.client.send_message(event.chat_id, response.message)
+        return
+    input_str = event.pattern_match.group(1)
+    reply = await event.get_reply_message()
+    if input_str:
+        quote = input_str
+    elif reply:
+        quote = reply
+    else:
+    	await event.edit("```Plz Reply to a TEXT message or Input TEXT.```")
+    return
+    bot = "@QuotLyBot"
+    await event.edit(f"```Making a Quote...```")
+
+    async with borg.conversation(bot) as bot_conv:
+        if True:
+            response = await silently_send_message(bot_conv, "/start")
+            if "Hi!" not in response.text:
+                await event.edit(f"{response.text}")
+                return
+            if input_str:
+              response = await silently_send_message(bot_conv, quote)
+            elif reply:
+              response = bot_conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
+              await borg.forward_messages(bot, quote)
+              response = await response
+              response = response.message
+            if response.text.startswith("Command"):
+                await event.edit(f"Invalid message type.")
+                return
+            await event.reply(response)
+            await event.delete()
+
+
+async def silently_send_message(conv, text):
+    await conv.send_message(text)
+    response = await conv.get_response()
+    await conv.mark_read(message=response)
+    return response
