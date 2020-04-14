94fe105e8dc2df6fc6db291590e4035461f0714e
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-04-14 17:26:18 +0530

    Update thinklol.py

--- userbot/plugins/thinklol.py
+++ userbot/plugins/thinklol.py
@@ -9,7 +9,7 @@ from uniborg.util import admin_cmd
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("🤔🧐🤔🧐🤔🧐"))
+	deq = deque(list("😂🤣😂🤣😂🤣"))
 	for _ in range(999):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
