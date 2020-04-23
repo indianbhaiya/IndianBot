0da4c02c00044c443de92aeffc819953dfce838f
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-04-23 09:07:18 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -246,7 +246,7 @@ def errors_handler(func):
 
             text = "**USERBOT CRASH REPORT**\n\n"
 
-            link = "[here](https://t.me/PaperplaneExtendedSupport)"
+            link = "[here](https://t.me/IndianArMyGiveaway)"
             text += "If you wanna you can report it"
             text += f"- just forward this message {link}.\n"
             text += "Nothing is logged except the fact of error and date\n"
