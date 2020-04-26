72c042483113e3e895cd0048406361aa75c7ac2b
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-04-26 16:43:39 +0530

    Update extra.py

--- userbot/plugins/extra.py
+++ userbot/plugins/extra.py
@@ -34,7 +34,7 @@ async def fun(e):
 async def Ooo(e):
     t = "yo"
     for j in range(15):
-        t = t[:-1] + "of"
+        t = t[:-1] + "oo"
         await e.edit(t)
 
 @borg.on(admin_cmd("Oof$"))
