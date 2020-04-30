fc3476292ec6901ce5e3664331eb54c6ee38d0d6
TERMUXID7 <55914808+BLUE-DEVIL1134@users.noreply.github.com>
2020-04-30 21:15:28 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -1,3 +1,4 @@
+import time
 import asyncio
 import io
 import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
@@ -13,10 +14,12 @@ PREV_REPLY_MESSAGE = {}
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 USER_BOT_WARN_ZERO = "`You were spamming my peru master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` "
 USER_BOT_NO_WARN = ("[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id=953414679)\n\n"
-                    "`Hello, this is IndianBot Security Service.You have found your way here to my master,`"
-                    f"{DEFAULTUSER}`'s inbox.\n\n"
-                    "Leave your name, phone number, address and 10k$ and hopefully you'll get a reply within 2 light years.`\n\n"
-                    "** Send** `/start` ** so that we can decide why you're here.**")
+                    "`Hello, This Is` **🇮🇳IndianBot🇮🇳 Security Service**."
+                    "**I Am Made By A Pro , I Found Ur Way Through "
+                    f"{DEFAULTUSER}'s inbox**.\n\n"
+                    "`🔶I Am :` **☣HOT🔥**"
+                    "**This Is Regarded As Illegal And A Crime😬**\n\n"
+                    "** So Send **/start** To Start A Valid Conv. ")
 
 
 if Var.PRIVATE_GROUP_ID is not None:
