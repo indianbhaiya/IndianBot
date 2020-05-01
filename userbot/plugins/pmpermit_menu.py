00b1853172fd8365cea9f50f228f6080b9ed19bf
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-05-01 22:03:20 +0530

    Update pmpermit_menu.py

--- userbot/plugins/pmpermit_menu.py
+++ userbot/plugins/pmpermit_menu.py
@@ -15,7 +15,7 @@ from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, join our channel @IndianArMyGiveaway"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in heroku vars"
 PREV_REPLY_MESSAGE = {}
 
 
@@ -29,23 +29,22 @@ async def _(event):
             return
         if event.is_private:
          
-         PM = ("**Hello. You are accessing 🇮🇳IndianBot🇮🇳 Menu**,"
-               f"🔷**U Are Talking With : **{DEFAULTUSER}.\n"
-               "**🙏Choose One Of The Following Reasons Of Your Appearance:**\n\n"
-               f"`1`. **To Chat With {DEFAULTUSER} 🇮🇳**\n"
-               "`2`. 🆘**To Spam My Home**.\n"
-               "`3`. **To Enquire **\n"
-               "`4`. **To Request **\n"
-               "`5`.**To Seek Help**\n"
-               "`6`.**To Hack Me**\n")
-         ONE = ("__👌 OK. Your Request To Chat Has Been Registered. Now Don't S.P.A.M.__\n\n")
+         PM = ("`Hello. You are accessing the availabe menu of my peru master,`"
+               f"{DEFAULTUSER}.\n"
+               "__Let's make this smooth and let me know why you are here.__\n"
+               "**Choose one of the following reasons why you are here:**\n\n"
+               "`1`. To chat with my master\n"
+               "`2`. To spam my master's inbox.\n"
+
+               "`3`. To enquire something\n"
+               "`4`. To request something\n")
+         ONE = ("__Okay. Your request has been registered. Do not spam my master's inbox.You can expect a reply within 24 light years. He is a busy man, unlike you probably.__\n\n"
                 "**⚠️ You will be blocked and reported if you spam nibba. ⚠️**\n\n"
                 "__Use__ `/start` __to go back to the main menu.__")
-         TWO = (" **███████▄▄███████████▄  \n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓███░░░░░░░░░░░░█\n██████▀▀▀█░░░░██████▀  \n░░░░░░░░░█░░░░█  \n░░░░░░░░░░█░░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░░▀▀ **\n\n**So uncool, this is not your home. Go bother someone else. You have been blocked and reported until further notice.**")
+         TWO = (" `███████▄▄███████████▄  \n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓███░░░░░░░░░░░░█\n██████▀▀▀█░░░░██████▀  \n░░░░░░░░░█░░░░█  \n░░░░░░░░░░█░░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░░▀▀ `\n\n**So uncool, this is not your home. Go bother someone else. You have been blocked and reported until further notice.**")
          FOUR = ("__Okay. My master has not seen your message yet.He usually responds to people,though idk about retarted ones.__\n __He'll respond when he comes back, if he wants to.There's already a lot of pending messages😶__\n **Please do not spam unless you wish to be blocked and reported.**")
          FIVE = ("`Okay. please have the basic manners as to not bother my master too much. If he wishes to help you, he will respond to you soon.`\n**Do not ask repeatdly else you will be blocked and reported.**")
          LWARN = ("**This is your last warning. DO NOT send another message else you will be blocked and reported. Keep patience. My master will respond you ASAP.**\n__Use__ `/start` __to go back to the main menu.__")
-         SEX = ("**ABE CHUTIYE , TERI OKAT KYA HE?  MERKO HACK KAREGA? **\n U Have Been FBan[Fuck Ban]")
      
         async with borg.conversation(chat) as conv:
          await borg.send_message(chat, PM)
@@ -99,11 +98,6 @@ async def _(event):
                      await borg.send_message(chat, TWO)
                      await asyncio.sleep(3)
                      await event.client(functions.contacts.BlockRequest(chat_id))
-         elif y == "6":
-            await brog.send_message(chat,SEX)
-            pop = await conv.get_response(chat)
-            if "ok" or "OK" or "Ok" in pop.text:
-                await brog.send_message(chat,"GOOD , NOW **FUCK OFF **")
          else:
              await borg.send_message(chat, "`You have entered an invalid command. Please send /start again or do not send another message if you do not wish to be blocked and reported.`")
              response = await conv.get_response(chat)
