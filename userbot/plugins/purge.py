b22364ef5cacc8b68dc1a330a56aaf856781a7fe
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-05-11 22:14:47 +0530

    fixed 😎

--- userbot/plugins/purge.py
+++ userbot/plugins/purge.py
@@ -13,7 +13,8 @@ from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.utils import register, errors_handler
 
 
-@register(outgoing=True, pattern="^.purge$")
+#@register(outgoing=True, pattern="^.purge$")
+@borg.on(admin_cmd(pattern=r"purge"))
 @errors_handler
 async def fastpurger(purg):
     """ For .purge command, purge all messages starting from the reply. """
@@ -45,7 +46,8 @@ async def fastpurger(purg):
     await done.delete()
 
 
-@register(outgoing=True, pattern="^.purgeme")
+#@register(outgoing=True, pattern="^.purgeme")
+@borg.on(admin_cmd(pattern=r"purgeme"))
 @errors_handler
 async def purgeme(delme):
     """ For .purgeme, delete x count of your latest message."""
@@ -73,7 +75,8 @@ async def purgeme(delme):
     await smsg.delete()
 
 
-@register(outgoing=True, pattern="^.del$")
+#@register(outgoing=True, pattern="^.del$")
+@borg.on(admin_cmd(pattern=r"del"))
 @errors_handler
 async def delete_it(delme):
     """ For .del command, delete the replied message. """
@@ -91,7 +94,8 @@ async def delete_it(delme):
                     BOTLOG_CHATID, "Well, I can't delete a message")
 
 
-@register(outgoing=True, pattern="^.edit")
+#@register(outgoing=True, pattern="^.edit")
+@borg.on(admin_cmd(pattern=r"edit"))
 @errors_handler
 async def editer(edit):
     """ For .editme command, edit your last message. """
@@ -111,7 +115,8 @@ async def editer(edit):
                                        "Edit query was executed successfully")
 
 
-@register(outgoing=True, pattern="^.sd")
+#@register(outgoing=True, pattern="^.sd")
+@borg.on(admin_cmd(pattern=r"sd"))
 @errors_handler
 async def selfdestruct(destroy):
     """ For .sd command, make seflf-destructable messages. """
