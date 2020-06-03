47ab6799928ba3eb3f3ceca64f09cfadde58b23c
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-06-03 05:17:10 -0700

    its imp

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -104,7 +104,7 @@ if ENV:
         MONGO_URI = os.environ.get("MONGO_URI", None)
         #Lydia API
         LYDIA_API = os.environ.get("LYDIA_API",None)
-        PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", None))
+        PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", False))
 else:
     class Config(object):
         DB_URI = None
