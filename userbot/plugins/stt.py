8f3fb3a09a5c52cb6dcd9b25c4ec76ec2959038a
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-06-05 09:16:18 +0530

    Update stt.py

--- userbot/plugins/stt.py
+++ userbot/plugins/stt.py
@@ -4,7 +4,7 @@ from telethon import events
 import requests
 import os
 from datetime import datetime
-from uniborg.util import admin_cmd
+from userbot.util import admin_cmd
 
 
 @borg.on(admin_cmd(pattern="stt (.*)"))
