4e943835432a1b1d876ac1975205bf85fb803f9b
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-06-07 18:07:24 +0530

    fixed

--- userbot/plugins/gmute.py
+++ userbot/plugins/gmute.py
@@ -12,6 +12,7 @@ async def startgmute(event):
     private = False
     if event.fwd_from:
         return
+    reply = await event.get_reply_message()
     user_id = reply.sender_id
     if user_id == (await borg.get_me()).id:	
         await event.edit(r"LoL. Why would I Gmute u. You are my owner")	
