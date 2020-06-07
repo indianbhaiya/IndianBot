3decc4355642ef35882e80b7ce7a0e28152bf6c6
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-06-07 16:54:49 +0530

    Update gmute.py

--- userbot/plugins/gmute.py
+++ userbot/plugins/gmute.py
@@ -11,11 +11,8 @@ from telethon import events
 async def startgmute(event):
     private = False
     if event.fwd_from:
-        return 
-        user = await bot.get_me()
-    if not user.first_name:
-        user.first_name = user.id
-        userid = event.from_id
+        return
+    userid = event.from_id
     elif user_id == (await borg.get_me()).id:	
         await event.edit(r"LoL. Why would I Gmute u. You are my owner")	
         return	
