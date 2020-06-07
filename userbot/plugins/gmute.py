93e657ede6738c1948017b80f51101ed282bb94e
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-06-07 17:11:46 +0530

    preventing from gmuting your self and fixed sudos gmuting

--- userbot/plugins/gmute.py
+++ userbot/plugins/gmute.py
@@ -12,7 +12,7 @@ async def startgmute(event):
     private = False
     if event.fwd_from:
         return
-    userid = event.from_id
+    user_id = event.from_id
     if user_id == (await borg.get_me()).id:	
         await event.edit(r"LoL. Why would I Gmute u. You are my owner")	
         return	
