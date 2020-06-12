a27045ed0858c4e9cc5d1fb78bc67731c0312acc
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-06-12 16:13:24 +0530

    Update test.py

--- userbot/plugins/test.py
+++ userbot/plugins/test.py
@@ -4,8 +4,8 @@ import os
 import sys
 from uniborg.util import admin_cmd
 
-@borg.on(admin_cmd(pattern=r"test"))
+@borg.on(admin_cmd(pattern="test ?(.*)", allow_sudo=True))
 async def test(event):
     if event.fwd_from:
         return 
-    await event.edit("Test Successfull. Ja Ja Gand mara")      
+    await event.reply("Test Successfull. Ja Ja Gand mara")
