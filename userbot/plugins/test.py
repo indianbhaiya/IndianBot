a08976a6f1e7806f86b9e3204b1fd5adabb9b8b3
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-06-14 10:01:52 +0530

    test

--- userbot/plugins/test.py
+++ userbot/plugins/test.py
@@ -6,6 +6,6 @@ from uniborg.util import admin_cmd
 
 @borg.on(admin_cmd(pattern="test ?(.*)", allow_sudo=True))
 async def test(event):
-    if event.fwd_from:
-        return 
-    await event.reply("Test Successfull. Ja Ja Gand mara")
+    chat = await event.get_chat()
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await borg.send_message(chat, "Test Successfull. Ja Ja Gand mara")
