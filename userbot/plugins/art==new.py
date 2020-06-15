b0e774260345614181a12b09485ebe3d2dbfdeed
TERMUXID7 <55914808+BLUE-DEVIL1134@users.noreply.github.com>
2020-06-15 15:51:28 +0530

    Update and rename newart to art==new.py

---
+++ userbot/plugins/art==new.py
@@ -0,0 +1,26 @@
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+
+n = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
+
+#@command(outgoing=True, pattern="^.ded$")
+@borg.on(admin_cmd(pattern=r"ded"))
+async def bluedevilded(ded):
+    await alive.edit("I ==             |\n　　　　　|" "\n　　　　　| \n"
+"　　　　　| \n"
+"　　　　　| \n"
+"　　　　　| \n"
+"　　　　　| \n"
+"　　　　　| \n"
+"　　　　　| \n"
+"　／￣￣＼| \n"
+"＜ ´･ 　　 |＼ \n"
+"　|　３　 | 丶＼ \n"
+"＜ 、･　　|　　＼ \n"
+"　＼＿＿／∪ _ ∪) \n"
+"　　　　　 Ｕ Ｕ\n")
