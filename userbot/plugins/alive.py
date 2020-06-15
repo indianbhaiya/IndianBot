70bd3e6d1b1d4c38b7a46c862a5e115c27d3ab89
TERMUXID7 <55914808+BLUE-DEVIL1134@users.noreply.github.com>
2020-06-15 12:00:20 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -15,7 +15,7 @@ async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**Apun Zinda He Sarr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     "`Telethon version:` **6.0.9**\n`Python:` **3.7.4**\n"
+                     "`Indian Bot Version:` [1.0](https://telegra.ph/INDIAN-06-15-6)\n`Python:` **3.7.4**\n"
                      "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
                      "**Bot Creator:** [🇮🇳INDIAN BHAI](t.me/pureindialover)\n"
                      "**Co-Owner:** [🇮🇳AKASH](t.me/AKASH_AM1)\n\n"
