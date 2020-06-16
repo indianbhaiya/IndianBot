b28ab61b6464c3421132abc38aecbfb22091b081
TERMUXID7 <55914808+BLUE-DEVIL1134@users.noreply.github.com>
2020-06-16 10:58:50 +0530

    Update _inimain.py

--- userbot/plugins/_inimain.py
+++ userbot/plugins/_inimain.py
@@ -21,7 +21,6 @@ async def _(event):
     if event.fwd_from:
         return
     sysarg = event.pattern_match.group(1)
-    
     if "." in sysarg :
      async with borg.conversation(bot) as conv:
           try:
@@ -33,7 +32,7 @@ async def _(event):
               await borg.send_message(event.chat_id, audio.text)
           except YouBlockedUserError:
               await event.edit("**Error:** `unblock` @CasperCarderBot `and retry!")
-     elif "" in sysarg :
+    elif " " in sysarg :
       async with borg.conversation(bot) as conv:
           try:
               await conv.send_message("/start")
