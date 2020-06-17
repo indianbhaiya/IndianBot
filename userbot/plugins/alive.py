66a2fb85975701be1bb406c2e6f82c3c906c1da0
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-06-17 11:07:10 +0530

    fix

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,7 +8,7 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-BLUE = ("**Apun Zinda He Sarr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
+INDIANBOT_IS_ALIVE = ("**Apun Zinda He Sarr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
                      "`Indian Bot Version:` [1.0](https://telegra.ph/INDIAN-06-15-6)\n`Python:` **3.7.4**\n"
                      "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
@@ -20,6 +20,7 @@ BLUE = ("**Apun Zinda He Sarr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
+    chat = await event.get_chat()
     await alive.delete()
     """ For .alive command, check if the bot is running.  """
-    await alive.send_message(alive.chat_id , BLUE) 
+    await borg.send_message(chat, INDIANBOT_IS_ALIVE) 
