ee429f98228949963e44639413065f3eff315740
TERMUXID7 <55914808+BLUE-DEVIL1134@users.noreply.github.com>
2020-06-17 10:02:57 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,6 +6,14 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
+BLUE = ("**Apun Zinda He Sarr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
+                     f"`My peru owner`: {DEFAULTUSER}\n\n"
+                     "`Indian Bot Version:` [1.0](https://telegra.ph/INDIAN-06-15-6)\n`Python:` **3.7.4**\n"
+                     "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
+                     "**Bot Creator:** [🇮🇳INDIAN BHAI](t.me/pureindialover)\n"
+                     "**Co-Owner:** [🇮🇳AKASH](t.me/AKASH_AM1)\n\n"
+                     "     [🇮🇳Deploy This IndianBot🇮🇳](https://github.com/indianbhaiya/IndianBot)") 
+
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
@@ -14,11 +22,4 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 async def amireallyalive(alive):
     await alive.delete()
     """ For .alive command, check if the bot is running.  """
-    await alive.send_message(alive.chat_id , "**Apun Zinda He Sarr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
-                     f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     "`Indian Bot Version:` [1.0](https://telegra.ph/INDIAN-06-15-6)\n`Python:` **3.7.4**\n"
-                     "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
-                     "**Bot Creator:** [🇮🇳INDIAN BHAI](t.me/pureindialover)\n"
-                     "**Co-Owner:** [🇮🇳AKASH](t.me/AKASH_AM1)\n\n"
-                     "     [🇮🇳Deploy This IndianBot🇮🇳](https://github.com/indianbhaiya/IndianBot)") 
-
+    await alive.send_message(alive.chat_id , BLUE) 
