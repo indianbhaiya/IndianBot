19e3c1153d3dda86b46d2583d15fd49f8b6fc66c
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-06-21 21:59:21 +0530

    Plugin Review!! [Made it Custom Cause of many requests]

--- userbot/plugins/call.py
+++ userbot/plugins/call.py
@@ -1,18 +1,14 @@
 """call
-
 Available Commands:
-
 .call
-
 """
 
 from telethon import events
-
 import asyncio
 from userbot.utils import admin_cmd
+from userbot import ALIVE_NAME
 
-
-
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@pureindialover"
 
 @borg.on(admin_cmd(pattern=r"call"))
 
@@ -34,24 +30,24 @@ async def _(event):
 
     animation_chars = [
         
-            "`Connecting To Telegram Headquarters...`",
-            "`Call Connected.`",
-            "`Telegram: Hello This is Telegram HQ. Who is this?`",
-            "`Me: Yo this is` @pureindialover ,`Please Connect me to my lil bro,Pavel Durov`",
-            "`User Authorised.`",
-            "`Calling Pavel Durov`  `At +916969696969`",
-            "`Private  Call Connected...`",
-            "`Me: Hello Sir, Please Ban This Telegram Account.`",    
-            "`Pavel: May I Know Who Is This?`",
-            "`Me: Yo Brah, I Am` @pureindialover ",
-            "`Pavel: OMG!!! Long time no see, Wassup Brother...\nI'll Make Sure That Guy Account Will Get Blocked Within 24Hrs.`",
-            "`Me: Thanks, See You Later Brah.`",
-            "`Pavel: Please Don't Thank Brah, Telegram Is Our's. Just Gimme A Call When You Become Free.`",
-            "`Me: Is There Any Issue/Emergency???`",
-            "`Pavel: Yes Sur, There Is A Bug In Telegram v69.6.9.\nI Am Not Able To Fix It. If Possible, Please Help Fix The Bug.`",
-            "`Me: Send Me The App On My Telegram Account, I Will Fix The Bug & Send You.`",
-            "`Pavel: Sure Sur \nTC Bye Bye :)`",
-            "`Private Call Disconnected.`"
+            "**Connecting To Telegram Headquarters...**",
+            "**Call Connected.**",
+            "**Telegram: Hello This is Telegram HQ. Who is this?**",
+            f"**Me: Yo this is** {DEFAULTUSER} ,**Please Connect me to my lil bro,Pavel Durov**",
+            "**User Authorised.**",
+            "**Calling Pavel Durov At +916969696969**",
+            "**Private  Call Connected...**",
+            "**Me: Hello Sir, Please Ban This Telegram Account.**",    
+            "**Pavel: May I Know Who Is This?**",
+            f"**Me: Yo Brah, I Am** {DEFAULTUSER}",
+            "**Pavel: OMG!!! Long time no see, Wassup Brother...**\n**I'll Make Sure That Guy Account Will Get Blocked Within 24Hrs.**",
+            "**Me: Thanks, See You Later Brah.**",
+            "**Pavel: Please Don't Thank Brah, Telegram Is Our's. Just Gimme A Call When You Become Free.**",
+            "**Me: Is There Any Issue/Emergency??**",
+            "**Pavel: Yes Sur, There Is A Bug In Telegram v69.6.9.**\n**I Am Not Able To Fix It. If Possible, Please Help Fix The Bug.**",
+            "**Me: Send Me The App On My Telegram Account, I Will Fix The Bug & Send You.**",
+            "**Pavel: Sure Sur**\n**TC Bye Bye :)**",
+            "**Private Call Disconnected.**"
         ]
 
     for i in animation_ttl:
