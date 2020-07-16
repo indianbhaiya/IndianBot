f7abeb0093f8070a438a9ac1e012009e25ce11c2
xditya <58950863+xditya@users.noreply.github.com>
2020-07-17 05:25:15 +0530

    people change ._.
    removed auto approve if otgoing 😒
    idgaf if uget into problems like userbots blocking each other and so on

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -41,22 +41,6 @@ if Var.PRIVATE_GROUP_ID is not None:
                 await event.edit("Approved to pm [{}](tg://user?id={})".format(firstname, chat.id))
                 await asyncio.sleep(3)
                 await event.delete()
-                
-    @bot.on(events.NewMessage(outgoing=True))
-    async def you_dm_niqq(event):
-        if event.fwd_from:
-            return
-        chat = await event.get_chat()
-        if event.is_private:
-            if not pmpermit_sql.is_approved(chat.id):
-                if not chat.id in PM_WARNS:
-                    pmpermit_sql.approve(chat.id, "outgoing")
-                    bruh = "**User added to approved list as out-going message**"
-                    rko = await borg.send_message(event.chat_id, bruh)
-                    await asyncio.sleep(3)
-                    await rko.delete()
-
-
 
     @command(pattern="^.block ?(.*)")
     async def approve_p_m(event):
