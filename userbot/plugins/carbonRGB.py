1e09fdb3b9f5b784ee8316cd701215b6f15d9d7a
TERMUXID7 <55914808+BLUE-DEVIL1134@users.noreply.github.com>
2020-07-18 17:03:48 +0530

    Update carbonRGB.py

--- userbot/plugins/carbonRGB.py
+++ userbot/plugins/carbonRGB.py
@@ -100,7 +100,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Carbonised by [IndianBot](https://t.me/IndianArMyGiveaway)",
+         caption="✅Carbonised by [IndianBot](https://github.com/indianbhaiya/IndianBot/)",
          force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
