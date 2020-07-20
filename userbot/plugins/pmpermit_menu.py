ee20183688084da9ae79991d03d4ec8f07179ae8
TERMUXID7 <55914808+BLUE-DEVIL1134@users.noreply.github.com>
2020-07-20 14:30:21 +0530

    Made The Vars to The Top To Avoid A Error

--- userbot/plugins/pmpermit_menu.py
+++ userbot/plugins/pmpermit_menu.py
@@ -15,9 +15,33 @@ from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in heroku vars"
+DEFAULTUSER = str(ALIVE_NAME)
 PREV_REPLY_MESSAGE = {}
+PM = ("`Hello. You are accessing the availabe menu of my peru master,`"
+               f"{DEFAULTUSER}.\n"
+               "__Let's make this smooth and let me know why you are here.__\n"
+               "**Choose one of the following reasons why you are here:**\n\n"
+               "`1`. To Chat With My Master.\n"
+               "`2`. To Spam My mMaster's Inbox.\n"
 
+               "`3`. To Enquire Something.\n"
+               "`4`. To Request Something.\n"
+               "`5`. To Gib Something.")
+ONE = ("__Okay. Your request has been registered. Do not spam my master's inbox.You can expect a reply within 24 light years. He is a busy man, unlike you probably.__\n\n"
+                "**⚠️ You will be blocked and reported if you spam nibba. ⚠️**\n\n"
+                "__Use__ `/start` __to go back to the main menu.__")
+TWO = ("▬▬▬.◙.▬▬▬ \n"
+       "═▂▄▄▓▄▄▂ \n"
+       "◢◤ █▀▀████▄▄▄▄◢◤ \n"
+       "█▄ █ █▄ ███▀▀▀▀▀▀▀╬ \n"
+       "◥█████◤ \n"
+       "══╩══╩══ \n")
+TW = (" `███████▄▄███████████▄  \n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓███░░░░░░░░░░░░█\n██████▀▀▀█░░░░██████▀  \n░░░░░░░░░█░░░░█  \n░░░░░░░░░░█░░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░░▀▀ `\n\n**So uncool, this is not your home. Go bother someone else. You have been blocked and reported until further notice.**")
+FOUR = ("__Okay. My master has not seen your message yet.He usually responds to people,though idk about retarted ones.__\n __He'll respond when he comes back, if he wants to.There's already a lot of pending messages😶__\n **Please do not spam unless you wish to be blocked and reported.**")
+FIVE = ("`Okay. please have the basic manners as to not bother my master too much. If he wishes to help you, he will respond to you soon.`\n**Do not ask repeatdly else you will be blocked and reported.**")
+LWARN = ("**This is your last warning. DO NOT send another message else you will be blocked and reported. Keep patience. My master will respond you ASAP.**\n__Use__ `/start` __to go back to the main menu.__")
+B = (".block")
+XYZ = ("HIYA BOSS! THANKS 😃\n LEAVE WHATEVER U WANT TO LEAVE HERE \n I AM GLAD FROM U BOI🔜\n. ◌⑅⃝●♡⋆♡LOVE♡⋆♡●⑅◌")
 
 @command(pattern=r"\/start", incoming=True)
 async def _(event):
@@ -28,35 +52,8 @@ async def _(event):
         if event.fwd_from:
             return
         if event.is_private:
-         
-         PM = ("`Hello. You are accessing the availabe menu of my peru master,`"
-               f"{DEFAULTUSER}.\n"
-               "__Let's make this smooth and let me know why you are here.__\n"
-               "**Choose one of the following reasons why you are here:**\n\n"
-               "`1`. To Chat With My Master.\n"
-               "`2`. To Spam My mMaster's Inbox.\n"
-
-               "`3`. To Enquire Something.\n"
-               "`4`. To Request Something.\n"
-               "`5`. To Gib Something.")
-         ONE = ("__Okay. Your request has been registered. Do not spam my master's inbox.You can expect a reply within 24 light years. He is a busy man, unlike you probably.__\n\n"
-                "**⚠️ You will be blocked and reported if you spam nibba. ⚠️**\n\n"
-                "__Use__ `/start` __to go back to the main menu.__")
-         TWO = ("▬▬▬.◙.▬▬▬ \n"
-                "═▂▄▄▓▄▄▂ \n"
-                "◢◤ █▀▀████▄▄▄▄◢◤ \n"
-                "█▄ █ █▄ ███▀▀▀▀▀▀▀╬ \n"
-                "◥█████◤ \n"
-                "══╩══╩══ \n")
-         TW = (" `███████▄▄███████████▄  \n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓███░░░░░░░░░░░░█\n██████▀▀▀█░░░░██████▀  \n░░░░░░░░░█░░░░█  \n░░░░░░░░░░█░░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░░▀▀ `\n\n**So uncool, this is not your home. Go bother someone else. You have been blocked and reported until further notice.**")
-         FOUR = ("__Okay. My master has not seen your message yet.He usually responds to people,though idk about retarted ones.__\n __He'll respond when he comes back, if he wants to.There's already a lot of pending messages😶__\n **Please do not spam unless you wish to be blocked and reported.**")
-         FIVE = ("`Okay. please have the basic manners as to not bother my master too much. If he wishes to help you, he will respond to you soon.`\n**Do not ask repeatdly else you will be blocked and reported.**")
-         LWARN = ("**This is your last warning. DO NOT send another message else you will be blocked and reported. Keep patience. My master will respond you ASAP.**\n__Use__ `/start` __to go back to the main menu.__")
-         B = (".block")
-         XYZ = ("HIYA BOSS! THANKS 😃\n LEAVE WHATEVER U WANT TO LEAVE HERE \n I AM GLAD FROM U BOI🔜\n. ◌⑅⃝●♡⋆♡LOVE♡⋆♡●⑅◌")
-     
         async with borg.conversation(chat) as conv:
-         await borg.send_message(chat, PM)
+            await borg.send_message(chat, PM)
          chat_id = event.from_id
          response = await conv.get_response(chat)
          y = response.text
