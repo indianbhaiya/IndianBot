a676b4707ef024da1d522a7e918b9921fd4e248a
Akash <akashjio66666@gmail.com>
2020-08-11 12:16:24 +0530

    Update eval.py

--- userbot/plugins/eval.py
+++ userbot/plugins/eval.py
@@ -1,18 +1,9 @@
 """Evaluate Python Code inside Telegram
 Syntax: .eval PythonCode"""
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-from telethon import events, errors, functions, types
-import inspect
 import traceback
-import asyncio
 import sys
 import io
 from uniborg.util import admin_cmd
-
-
 @borg.on(admin_cmd("eval"))
 async def _(event):
     if event.fwd_from:
@@ -22,23 +13,19 @@ async def _(event):
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = io.StringIO()
     redirected_error = sys.stderr = io.StringIO()
     stdout, stderr, exc = None, None, None
-
     try:
         await aexec(cmd, event)
     except Exception:
         exc = traceback.format_exc()
-
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
-
     evaluation = ""
     if exc:
         evaluation = exc
@@ -48,9 +35,7 @@ async def _(event):
         evaluation = stdout
     else:
         evaluation = "Success"
-
     final_output = "**EVAL**: `{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
-
     if len(final_output) > Config.MAX_MESSAGE_SIZE_LIMIT:
         with io.BytesIO(str.encode(final_output)) as out_file:
             out_file.name = "eval.text"
@@ -65,8 +50,6 @@ async def _(event):
             await event.delete()
     else:
         await event.edit(final_output)
-
-
 async def aexec(code, event):
     exec(
         f'async def __aexec(event): ' +
