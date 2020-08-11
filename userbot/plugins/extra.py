4bc0f22b4cfd7eecd414e775ba5b74409bd49d39
Akash <akashjio66666@gmail.com>
2020-08-11 11:07:57 +0530

    Update extra.py

--- userbot/plugins/extra.py
+++ userbot/plugins/extra.py
@@ -1,15 +1,10 @@
-import asyncio, subprocess
-import time, re, io
+import asyncio
+import time
 from userbot import bot, BOTLOG, BOTLOG_CHATID, CMD_HELP
-from telethon import events, functions, types
-from telethon.events import StopPropagation
-from telethon.tl.functions.messages import ExportChatInviteRequest
-from telethon.tl.functions.contacts import BlockRequest
-from telethon.tl.functions.channels import LeaveChannelRequest, CreateChannelRequest, DeleteMessagesRequest
+from telethon.tl.functions.channels import LeaveChannelRequest
 from collections import deque
-from telethon.tl.functions.users import GetFullUserRequest
-from userbot.events import register
 from userbot.utils import admin_cmd
+
 
 @borg.on(admin_cmd("leave$"))
 async def leave(e):
@@ -21,90 +16,86 @@ async def leave(e):
         else:
             await e.edit('`Sar This is Not A Chat`')
 
+
 @borg.on(admin_cmd(";__;$"))
-#@register(outgoing=True, pattern="^;__;$")
 async def fun(e):
     t = ";__;"
     for j in range(10):
         t = t[:-1] + "_;"
         await e.edit(t)
 
+
 @borg.on(admin_cmd("yo$"))
-#@register(outgoing=True, pattern="^yo$")
 async def Ooo(e):
     t = "yo"
     for j in range(15):
         t = t[:-1] + "oo"
         await e.edit(t)
 
+
 @borg.on(admin_cmd("Oof$"))
-#@register(outgoing=True, pattern="^Oof$")
 async def Oof(e):
     t = "Oof"
     for j in range(15):
         t = t[:-1] + "of"
         await e.edit(t)
 
+
 @borg.on(admin_cmd("ccry$"))
-#@register(outgoing=True, pattern="^.cry$")
 async def cry(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("(;´༎ຶД༎ຶ)")
 
+
 @borg.on(admin_cmd("fp$"))
-#@register(outgoing=True, pattern="^.fp$")
 async def facepalm(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("🤦‍♂")
 
+
 @borg.on(admin_cmd("moon$"))
-#@register(outgoing=True, pattern="^.mmoon$")
 async def _(event):
-	if event.fwd_from:
-		return
-	deq = deque(list("🌗🌘🌑🌒🌓🌔🌕🌖"))
-	for _ in range(32):
-		await asyncio.sleep(0.1)
-		await event.edit("".join(deq))
-		deq.rotate(1)
-		
+    if event.fwd_from:
+        return
+    deq = deque(list("🌗🌘🌑🌒🌓🌔🌕🌖"))
+    for _ in range(32):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
 
 @borg.on(admin_cmd("source$"))
-#@register(outgoing=True, pattern="^.source$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("https://github.com/indianbhaiya/IndianBot")
 
+
 @borg.on(admin_cmd("readme$"))
-#@register(outgoing=True, pattern="^.readme$")
 async def reedme(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("https://github.com/indianbhaiya/IndianBot/blob/master/README.md")
 
 
-
-@borg.on(admin_cmd("heart$"))		
-#@register(outgoing=True, pattern="^.heart$")
+@borg.on(admin_cmd("heart$"))
 async def _(event):
-	if event.fwd_from:
-		return
-	deq = deque(list("❤️🧡💛💚💙💜🖤"))
-	for _ in range(32):
-		await asyncio.sleep(0.1)
-		await event.edit("".join(deq))
-		deq.rotate(1)
-		
+    if event.fwd_from:
+        return
+    deq = deque(list("❤️🧡💛💚💙💜🖤"))
+    for _ in range(32):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
 @borg.on(admin_cmd("fap$"))
-#@register(outgoing=True, pattern="^.fap$")
 async def _(event):
-	if event.fwd_from:
-		return
-	deq = deque(list("🍆✊🏻💦"))
-	for _ in range(32):
-		await asyncio.sleep(0.1)
-		await event.edit("".join(deq))
-		deq.rotate(1)
-
+    if event.fwd_from:
+        return
+    deq = deque(list("🍆✊🏻💦"))
+    for _ in range(32):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
 
 
 CMD_HELP.update({
