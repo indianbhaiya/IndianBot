85fec9431d7e50698a97713b964b680b7b87abbb
Akash <akashjio66666@gmail.com>
2020-08-11 12:19:05 +0530

    Update exec.py

--- userbot/plugins/exec.py
+++ userbot/plugins/exec.py
@@ -1,11 +1,7 @@
-from telethon import events, errors, functions, types
-import inspect
 import traceback
-import asyncio
 import sys
 import io
 from userbot.utils import admin_cmd
-
 @borg.on(admin_cmd("exec"))
 async def _(event):
     if event.fwd_from:
@@ -15,23 +11,19 @@ async def _(event):
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = io.StringIO()
     redirected_error = sys.stderr = io.StringIO()
     stdout, stderr, exc = None, None, None
-
     try:
         await aexec(cmd, event)
     except Exception:
         exc = traceback.format_exc()
-
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
-
     evaluation = ""
     if exc:
         evaluation = exc
@@ -41,9 +33,7 @@ async def _(event):
         evaluation = stdout
     else:
         evaluation = "Success"
-
     final_output = "**EXEC**: `{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
-
     if len(final_output) > 4096:
         with io.BytesIO(str.encode(final_output)) as out_file:
             out_file.name = "eval.text"
@@ -58,8 +48,6 @@ async def _(event):
             await event.delete()
     else:
         await event.edit(final_output)
-
-
 async def aexec(code, event):
     exec(
         f'async def __aexec(event): ' +
