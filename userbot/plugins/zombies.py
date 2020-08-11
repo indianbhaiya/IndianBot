f6861f22d6b62b818918a02b820947196ec1c5da
Akash <akashjio66666@gmail.com>
2020-08-11 11:32:50 +0530

    Update zombies.py

--- userbot/plugins/zombies.py
+++ userbot/plugins/zombies.py
@@ -2,29 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-"""Cmd= `.zombie`
-Usage: Searches for deleted accounts in a groups and channels.
-Use .zombies clean to remove deleted accounts from the groups and channels.
-\nPorted by ©[NIKITA](t.me/kirito6969) and ©[EYEPATCH](t.me/NeoMatrix90)"""
-
+"""  Cmd = .zombie  """
 from telethon import events
 from userbot.utils import admin_cmd
-#
 from asyncio import sleep
-from os import remove
-
-from telethon.errors import (BadRequestError, ChatAdminRequiredError,
-                             ImageProcessFailedError, PhotoCropSizeSmallError,
-                             UserAdminInvalidError)
-from telethon.errors.rpcerrorlist import (UserIdInvalidError,
-                                          MessageTooLongError)
-from telethon.tl.functions.channels import (EditAdminRequest,
-                                            EditBannedRequest,
-                                            EditPhotoRequest)
-from telethon.tl.types import (ChannelParticipantsAdmins, ChatAdminRights,
-                               ChatBannedRights, MessageEntityMentionName,
-                               MessageMediaPhoto)
-
+from telethon.errors import (ChatAdminRequiredError, UserAdminInvalidError)
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights
 
 # =================== CONSTANT ===================
 
@@ -57,16 +41,13 @@ UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
 # ================================================
 
 
-
 @borg.on(admin_cmd(pattern=f"zombies", allow_sudo=True))
 @borg.on(events.NewMessage(pattern="^.zombies(?: |$)(.*)", outgoing=True))
 async def rm_deletedacc(show):
     """ For .zombies command, list all the ghost/deleted/zombie accounts in a chat. """
-
     con = show.pattern_match.group(1).lower()
     del_u = 0
     del_status = "`No deleted accounts found, Group is clean`"
-
     if con != "clean":
         await show.edit("`Searching for ghost/deleted/zombie accounts...`")
         async for user in show.client.iter_participants(show.chat_id):
@@ -79,21 +60,15 @@ async def rm_deletedacc(show):
             \nclean them by using .zombies clean`"
         await show.edit(del_status)
         return
-
-    # Here laying the sanity check
     chat = await show.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
-
-    # Well
     if not admin and not creator:
         await show.edit("`I am not an admin here!`")
         return
-
     await show.edit("`Deleting deleted accounts...\nOh I can do that?!?!`")
     del_u = 0
     del_a = 0
-
     async for user in show.client.iter_participants(show.chat_id):
         if user.deleted:
             try:
@@ -108,21 +83,14 @@ async def rm_deletedacc(show):
             await show.client(
                 EditBannedRequest(show.chat_id, user.id, UNBAN_RIGHTS))
             del_u += 1
-
-
     if del_u > 0:
         del_status = f"Cleaned **{del_u}** deleted account(s)"
-
     if del_a > 0:
         del_status = f"Cleaned **{del_u}** deleted account(s) \
         \n**{del_a}** deleted admin accounts are not removed"
-
-
     await show.edit(del_status)
     await sleep(2)
     await show.delete()
-
-
     if Config.G_BAN_LOGGER_GROUP is not None:
         await show.client.send_message(
             Config.G_BAN_LOGGER_GROUP, "#CLEANUP\n"
