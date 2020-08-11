21b445ed993c4aab7b7ea4a9a8fcb6072f8ead8f
Akash <akashjio66666@gmail.com>
2020-08-11 11:52:24 +0530

    Update google.py

--- userbot/plugins/google.py
+++ userbot/plugins/google.py
@@ -3,25 +3,11 @@ Available Commands:
 .go <query> credits to owner of bot
 """
 
-import asyncio
-import os
 from re import findall
-import requests
-from bs4 import BeautifulSoup
-from datetime import datetime
-from requests import get
-from urllib.parse import quote_plus
-from urllib.error import HTTPError
-from google_images_download import google_images_download
-from gsearch.googlesearch import search
 from userbot.utils import admin_cmd
 from search_engine_parser import GoogleSearch
-
-
 def progress(current, total):
     logger.info("Downloaded {} of {}\nCompleted {}".format(current, total, (current / total) * 100))
-
-
 @borg.on(admin_cmd("go (.*)"))
 async def gsearch(q_event):
     """ For .google command, do a Google search. """
